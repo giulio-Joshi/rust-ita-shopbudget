325681c65e411be2c38c0bf24f19284af70b90e3
giulio-Joshi <me@joshi.it>
2021-10-10 16:07:03 +0200

    Mappatura secca senza propagazione errori

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,44 @@
 use budgetteer::budget_optimize;
 
+use std::io::{self, Error};
 
 pub mod budgetteer;
 
+
 fn main() {
 
-    // TODO stdin input
+    let keyb_name = String::from("keyboards");
+    let usb_name = String::from("usb drivers");
 
-    let keybs = vec![30, 60,22, 55 ];
-    let drivers : Vec<u64> = vec![12,33,65];
+    let keyb_prices  = require( &keyb_name);
+    let drivers = require( &usb_name );
 
-    if let Ok(budget) = budget_optimize(72, keybs, drivers){
+    if let Ok(budget) = budget_optimize(72, keyb_prices, drivers){
         println!("{}", budget );
     } else  {
         println!("-1");
     }
+}
+
+fn require( item: &str ) -> Vec<u64> {
+    println!("Insert space separate cost of {}: \n", item);
+    let mut item_list = String::new();
+    if io::stdin().read_line(&mut item_list).unwrap() < 1 {
+        panic!("No way this is a {} price list", item);
+    }
+
+    str_to_vec(&item_list)
+}
+
+
+fn str_to_vec( origin : &str ) -> Vec<u64> {
+
+    println!("Trying to split {:?} ", origin);
+
+    origin.trim()
+        .split(' ')
+        .map(|x| x.replace(" ", "")
+            .parse::<u64>().unwrap())
+        .collect()
+
 }
